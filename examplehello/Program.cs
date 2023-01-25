10596e5da0ee8b51b4aba3c4571eb833fda9002f
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-01-25 14:51:26 +0300

    added second exanple - hello user

--- examplehello/Program.cs
+++ examplehello/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! _2");
+﻿string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
